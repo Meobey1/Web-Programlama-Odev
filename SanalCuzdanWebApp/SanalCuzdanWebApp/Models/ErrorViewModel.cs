d522cce5a39de2b2cfb4cc702e81f52a0aa25027
Meobey1 <mehmet.oruc@ogr.sakarya.edu.tr>
2022-12-27 22:13:41 +0300

    Gün1
    Yeni Proje başlangıç. Modeller oluşturdum. Migration yaptım. Bu projede amaç Sanal Cüzdan yapmak.

---
+++ SanalCuzdanWebApp/SanalCuzdanWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SanalCuzdanWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
